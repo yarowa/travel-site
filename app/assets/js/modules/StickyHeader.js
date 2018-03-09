bf38cf7f70c598a9757cbfb8cdeb427657fde266
Abdurahman Yarow <eng.yarow@gmail.com>
2018-03-09 19:59:30 +0100

    added smooth scroll to the navigation

--- app/assets/js/modules/StickyHeader.js
+++ app/assets/js/modules/StickyHeader.js
@@ -1,10 +1,18 @@
 import $ from 'jquery';
+import smoothScroll from 'jquery-smooth-scroll';
 import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
 class StickyHeader {
     constructor(){
         this.siteHeader = $(".site-header");
         this.headerTriggerElement = $(".large-hero__title");
+        this.pageSection = $(".page-section");
+        this.navigation = $(".primary-nav a");
         this.createHeaderWaypoint();
+        this.createPageSectionWaypoint();
+        this.addSmoothScroll();
+    }
+    addSmoothScroll(){
+        this.navigation.smoothScroll();
     }
     createHeaderWaypoint(){
         const that = this;
@@ -19,6 +27,36 @@ class StickyHeader {
                }
            },
             offset: "-15%"
+        });
+    }
+
+    createPageSectionWaypoint(){
+        const nav = this;
+        this.pageSection.each(function () {
+            const currentPageSection = this;
+            new Waypoint({
+              element: currentPageSection,
+              handler: function (direction) {
+                  if (direction == "down"){
+                      const smoothlink = currentPageSection.getAttribute("data-matching");
+                      nav.navigation.removeClass("current-link");
+                      $(smoothlink).addClass("current-link");
+                  }
+              },
+                offset: "18%"
+           });
+
+            new Waypoint({
+                element: currentPageSection,
+                handler: function (direction) {
+                    if (direction == "up"){
+                        const smoothlink = currentPageSection.getAttribute("data-matching");
+                        nav.navigation.removeClass("current-link");
+                        $(smoothlink).addClass("current-link");
+                    }
+                },
+                offset: "-40%"
+            });
         });
     }
 }
